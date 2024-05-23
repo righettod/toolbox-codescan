f8ca586bf53c3a947e894f75de82fe4f9e08046d
Dominique RIGHETTO <righettod@users.noreply.github.com>
2024-05-23 13:43:25 +0200

    Update scan-secrets.sh

--- scripts/scan-secrets.sh
+++ scripts/scan-secrets.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
 ################################################
-# Script to scan the current folder using 
+# Script to scan the current folder using
 # gitleaks to find credentials/secrets/...
 ################################################
+rm leaks-gitfiles.json leaks-sourcefiles.json 2>/dev/null
 if [ -d ".git" ]; then
-    gitleaks detect --config /tools/gitleaks-custom-config.toml --no-banner --report-format json --report-path leaks-gitfiles.json --source $(pwd) --verbose
+    gitleaks detect --config /tools/gitleaks-custom-config.toml --no-banner --report-format json --report-path /tmp/leaks-gitfiles.json --source $(pwd) --verbose
 fi
-gitleaks detect --no-git --config /tools/gitleaks-custom-config.toml --no-banner --report-format json --report-path leaks-sourcefiles.json --source $(pwd) --verbose
+gitleaks detect --no-git --config /tools/gitleaks-custom-config.toml --no-banner --report-format json --report-path /tmp/leaks-sourcefiles.json --source $(pwd) --verbose
+mv /tmp/leaks-* .
