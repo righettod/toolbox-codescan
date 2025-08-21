52201a0b4bbfb5b15c54c67e808678784d1db6ec
righettod <dominique.righetto@gmail.com>
2025-08-21 11:09:00 +0200

    Work on #2

--- misc/vulnerable-codebase/python/vulns00.py
+++ misc/vulnerable-codebase/python/vulns00.py
@@ -12,12 +12,21 @@ def realOne00():
     return content
 
 
+@app.route('/realOne01', methods=['GET'])
+def realOne01():
+    # Real reflected XSS with ineffective sanitization
+    my_param2 = flask.request.args.get('my_param')
+    my_param2 = my_param2.replace("/>", "")
+    content = f"<html><body>Hello {my_param2}</body></html>"
+    return content
+
+
 @app.route('/fakeOne00', methods=['GET'])
 def fakeOne00():
     # Fake reflected XSS
-    my_param2 = flask.request.args.get('my_param')
-    my_param2 = re.sub(r'[<>\'"]+', '', my_param2)
-    content = f"<html><body>Hello {my_param2}</body></html>"
+    my_param3 = flask.request.args.get('my_param')
+    my_param3 = re.sub(r'[<>\'"]+', '', my_param3)
+    content = f"<html><body>Hello {my_param3}</body></html>"
     return content
 
 
