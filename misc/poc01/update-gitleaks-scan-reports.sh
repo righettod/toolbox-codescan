5c5db1cac0066c6f13a12445fcff3f7a0845f601
righettod <dominique.righetto@gmail.com>
2025-09-28 10:06:17 +0200

    Work on #4

--- misc/poc01/update-gitleaks-scan-reports.sh
+++ misc/poc01/update-gitleaks-scan-reports.sh
@@ -9,6 +9,7 @@ wget -q -O /tmp/gl.tgz $GITLEAKS_RELEASE_LOCATION
 wget -q -O /tmp/rules.toml $GITLEAKS_RULES_LOCATION
 tar -xzf /tmp/gl.tgz -C /tmp
 chmod +x * /tmp/gitleaks
-/tmp/gitleaks detect --no-git --config /tmp/rules.toml --no-banner --report-format json --report-path findings.json --source "$(pwd)/vulnerable-codebase" --verbose
+/tmp/gitleaks detect --no-git --config /tmp/rules.toml --no-banner --report-format json --report-path "$(pwd)/findings.json" --source "$(pwd)/vulnerable-codebase" --verbose
+pwd
 ls -l findings.json
 exit 0
