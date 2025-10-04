a728fc19ee38afc96a3619ea1d06e0cfe1cdb793
righettod <dominique.righetto@gmail.com>
2025-10-04 10:18:59 +0200

    Work on #4

--- misc/poc01/update-gitleaks-scan-reports.sh
+++ misc/poc01/update-gitleaks-scan-reports.sh
@@ -5,8 +5,14 @@
 ###################################################
 GITLEAKS_RELEASE_LOCATION="https://github.com/gitleaks/gitleaks/releases/download/v8.28.0/gitleaks_8.28.0_linux_x64.tar.gz"
 GITLEAKS_RULES_LOCATION="https://raw.githubusercontent.com/righettod/toolbox-pentest-web/refs/heads/master/templates/gitleaks-custom-config.toml"
+# Download a release of GitLeaks
 wget -q -O /tmp/gl.tgz $GITLEAKS_RELEASE_LOCATION
-wget -q -O /tmp/rules.toml $GITLEAKS_RULES_LOCATION
+# Setup the set of rules used by GitLeaks
+## Use my common set of rules
+# wget -q -O /tmp/rules.toml $GITLEAKS_RULES_LOCATION
+## Use a local set of rules
+cp gitleaks-custom-config.toml /tmp/rules.toml
+# Scan
 tar -xzf /tmp/gl.tgz -C /tmp
 chmod +x * /tmp/gitleaks
 /tmp/gitleaks detect --no-git --config /tmp/rules.toml --no-banner --report-format json --report-path "$(pwd)/findings.json" --source "$(pwd)/vulnerable-codebase" --verbose
